2d0794443aef2c324a56d875eac488a2482eeaa2
Maéva Cavaleri <cavalerimaeva@gmail.com>
2026-01-08 15:22:10 +0100

    Update arbitrage.py

--- core/arbitrage.py
+++ core/arbitrage.py
@@ -7,23 +7,23 @@ class ArbitrageScanner :
   """
 
   def __init__(self) : 
-  #initialisation de la calculatrice de prix juste 
-  self.pricer=ForwardPricer() 
+    #initialisation de la calculatrice de prix juste 
+    self.pricer=ForwardPricer() 
 
   def scan(self,spot,market_price,r,tau):
     """
     Compare le prix affiché sur le marché (market_price) avec le prix juste théorique.
     """
     #1. Calcul du prix que le marché devrait afficher 
-    theoritical_price=self.pricer.calculate_forward_price(spot,r,tau)
+    theoretical_price=self.pricer.calculate_forward_price(spot,r,tau)
     #2. Comparaison 
     # CAS 1 : Le marché est trop cher (K>St/Z)
-    if market_price>theoritical_price :
-      gain=market_price-theoritical_price
+    if market_price>theoretical_price :
+      gain=market_price-theoretical_price
       return f"Opportunité d'arbitrage ! Le prix est trop haut. Profit potentiel : {gain:.2f}€"
     # CAS 2 : Le marché est trop bas (K<St/Z)
-    if market_price<theoritical_price :
-      gain=theoritical_price-market_price
+    elif market_price<theoretical_price :
+      gain=theoretical_price-market_price
       return f"Opportunité d'arbitrage ! Le prix est trop bas. Profit potentiel : {gain:.2f}€"
     else :
       return "Le prix est juste. Pas d'arbitrage possible."
